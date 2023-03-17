857acccca0813b0bfb8495812886e9f5f851987e
Tony Ruiz <tonyruizo@pm.me>
2023-03-16 21:31:03 -0400

    Update files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import "./App.css";
+import Header from "./components/Header";
+import Nav from "./components/Nav";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
-      <h1>Homepage</h1>
+      <Header />
+      <Nav />
+      <Main />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Nav.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Logo from "../assets/img/Logo.svg";
+
+const Nav = () => {
+  return (
+    <div>
+      <img src={Logo} alt="Logo for the restaurant" />
+    </div>
+  );
+};
+
+export default Nav;
