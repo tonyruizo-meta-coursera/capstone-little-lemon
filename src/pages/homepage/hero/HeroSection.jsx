12eb46d795fd58486048775187bfa60108f8c3c1
Tony Ruiz <tonyruizo@pm.me>
2023-03-27 19:46:57 -0400

    Modify Hero Section

--- src/pages/homepage/hero/HeroSection.jsx
+++ src/pages/homepage/hero/HeroSection.jsx
@@ -1,28 +1,33 @@
 import React from "react";
 import './heroSection.css';
+import restaurant from '../../../assets/img-misc/restauranfood.jpg';
 
 const HeroSection = () => {
   return (
     <section id="hero-section">
       <div className="hero-container">
         <div className="hero-info-container">
-          <div className="hero-info-heading">
+          <h1 className="hero-info-heading">
             Little Lemon
-          </div>
-          <div className="hero-info-subHeading">
+          </h1>
+          <h3 className="hero-info-subHeading">
             Chicago
-          </div>
+          </h3>
           <div className="hero-info-text">
             Lorem ipsum dolor sit amet consectetur adipisicing elit.
             Harum architecto illo doloremque, corporis provident eaque aperiam ab! Neque quisquam molestiae molestias vel!
             Eius ab earum necessitatibus, iusto recusandae quisquam hic provident doloremque fuga fugiat
             error sunt, ad neque dignissimos nam.
           </div>
+          <div className="hero-btn">
+            Reserve a Table
+          </div>
         </div>
-        <div className="hero-btn">
-          Reserve a Table
+        <div className="hero-image-container">
+          <div className="hero-img-item">
+            <img src={restaurant} alt="special menu dish" />
+          </div>
         </div>
-        <div className="hero-image-container"></div>
       </div>
     </section>
   );
