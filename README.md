a59372854b360a11c0b8331b4c72d8a0de5562f8
Tonyruizo <tonyruizo@pm.me>
2023-05-20 12:26:59 -0400

    Add screenshots, update readme

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
 # Little Lemon Restaurant's Web App
 
+A modern front-end landing page for a restaurant using ReactJS.
+
 #### Tools:
 
 - Figma
 - HTML, CSS, JSX, ReactJS
 - Formik, Yup
+- Media queries for Mobile view
 
 ---
 
@@ -18,3 +21,11 @@
 <img src="src/assets/screenshots/component-screenshot.png" width="400" height="200" />
 <img src="src/assets/screenshots/pre-apply-design-screenshot.png" width="450" height="200" />
 <img src="src/assets/screenshots/apply-design-screenshot.png" width="450" height="200" />
+
+---
+
+### Finished product
+
+<img src="src/assets/screenshots/little-lemon-page.png" width="250" height="200" />
+<img src="src/assets/screenshots/little-lemon-mobile-page.png" width="250" height="200" />
+<img src="src/assets/screenshots/little-lemon-validation.png" width="250" height="280" />
--- src/pages/bookings/BookingPage.jsx
+++ src/pages/bookings/BookingPage.jsx
@@ -26,94 +26,96 @@ const BookingPage = () => {
     }
   });
   return (
-    <div className="form-container">
-      <div className="form-heading">
-        <h1>Reserve a Table</h1>
+    <section id='formPage-section'>
+      <div className="form-container">
+        <div className="form-heading">
+          <h1>Reserve a Table</h1>
+        </div>
+        <div className="form-table">
+          <form onSubmit={formik.handleSubmit}>
+            <div className="input-container">
+              <label htmlFor="occassion">Select a date: </label>
+              <input
+                type="date"
+                id='date'
+                name='date'
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.date}
+              />
+            </div>
+            <div className="form-msg">
+              {formik.touched.date && formik.errors.date ? <p>{formik.errors.date}</p> : null}
+            </div>
+            <div className="input-container">
+              <label htmlFor="occassion">Select occassion: </label>
+              <select
+                id='select'
+                name='select'
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.select}
+              >
+                <option value='' disabled></option>
+                <option value="birthday">Birthday</option>
+                <option value="engagement">Engagement</option>
+                <option value="anniversary">Anniversary</option>
+                <option value="other">Other</option>
+              </select>
+            </div>
+            <div className="form-msg">
+              {formik.touched.select && formik.errors.select ? <p>{formik.errors.select}</p> : null}
+            </div>
+            <div className="input-container">
+              <label htmlFor="guest">Number of guest: </label>
+              <input
+                type='number'
+                placeholder='1-10'
+                name='guest'
+                id='guest'
+                value={formik.values.guest}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+              />
+            </div>
+            <div className="form-msg">
+              {formik.touched.guest && formik.errors.guest ? <p>{formik.errors.guest}</p> : null}
+            </div>
+            <div className="input-container">
+              <label htmlFor="email">Enter name: </label>
+              <input
+                id='name'
+                name='name'
+                placeholder='First and last'
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.name}
+              />
+            </div>
+            <div className="form-msg">
+              {formik.touched.name && formik.errors.name ? <p>{formik.errors.name}</p> : null}
+            </div>
+            <div className="input-container">
+              <label htmlFor="email">Enter email: </label>
+              <input
+                id='email'
+                name='email'
+                placeholder='Email'
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.email}
+              />
+            </div>
+            <div className="form-msg">
+              {formik.touched.email && formik.errors.email ? <p>{formik.errors.email}</p> : null}
+            </div>
+            <div className="form-btn">
+              <PrimaryBtn>Reserve</PrimaryBtn>
+            </div>
+          </form>
+        </div>
       </div>
-      <div className="form-table">
-        <form onSubmit={formik.handleSubmit}>
-          <div className="input-container">
-            <label htmlFor="occassion">Select a date: </label>
-            <input
-              type="date"
-              id='date'
-              name='date'
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-              value={formik.values.date}
-            />
-          </div>
-          <div className="form-msg">
-            {formik.touched.date && formik.errors.date ? <p>{formik.errors.date}</p> : null}
-          </div>
-          <div className="input-container">
-            <label htmlFor="occassion">Select occassion: </label>
-            <select
-              id='select'
-              name='select'
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-              value={formik.values.select}
-            >
-              <option value='' disabled></option>
-              <option value="birthday">Birthday</option>
-              <option value="engagement">Engagement</option>
-              <option value="anniversary">Anniversary</option>
-              <option value="other">Other</option>
-            </select>
-          </div>
-          <div className="form-msg">
-            {formik.touched.select && formik.errors.select ? <p>{formik.errors.select}</p> : null}
-          </div>
-          <div className="input-container">
-            <label htmlFor="guest">Number of guest: </label>
-            <input
-              type='number'
-              placeholder='1-10'
-              name='guest'
-              id='guest'
-              value={formik.values.guest}
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-            />
-          </div>
-          <div className="form-msg">
-            {formik.touched.guest && formik.errors.guest ? <p>{formik.errors.guest}</p> : null}
-          </div>
-          <div className="input-container">
-            <label htmlFor="email">Enter name: </label>
-            <input
-              id='name'
-              name='name'
-              placeholder='First and last'
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-              value={formik.values.name}
-            />
-          </div>
-          <div className="form-msg">
-            {formik.touched.name && formik.errors.name ? <p>{formik.errors.name}</p> : null}
-          </div>
-          <div className="input-container">
-            <label htmlFor="email">Enter email: </label>
-            <input
-              id='email'
-              name='email'
-              placeholder='Email'
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-              value={formik.values.email}
-            />
-          </div>
-          <div className="form-msg">
-            {formik.touched.email && formik.errors.email ? <p>{formik.errors.email}</p> : null}
-          </div>
-          <div className="form-btn">
-            <PrimaryBtn>Reserve</PrimaryBtn>
-          </div>
-        </form>
-      </div>
-    </div>
+    </section>
   );
 };
 
